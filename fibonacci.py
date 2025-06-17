548769579fb71d9099ca73de401baf3757725dc3
Andres Rios Tascon <ariostas@gmail.com>
2025-06-17 13:26:27 -0400

    Added fibonacci code

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+def fib(n):
+    if n < 0:
+        raise ValueError("Input must be positive")
+
+    a = 0
+    b = 1
+
+    # Check if n is equal to 0
+    if n in (0, 1):
+        return n
+      
+    for i in range(1, n):
+        c = a + b
+        a = b
+        b = c
+
+    return b
